53b798b1687f6bb8debdc62a51e910448b9a6da0
cell04 <eazylle04concepcion@gmail.com>
2017-09-21 15:10:32 +0800

    Test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Registrion
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+
+Route::get('/home', 'HomeController@index')->name('home');
